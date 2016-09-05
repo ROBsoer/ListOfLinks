1e579dda46c5ba1b30017c3ebadb93d98fd213c3
soer <soer.rob@gmail.com>
2016-09-05 16:57:35 +0300

    some refactoring

--- PocketExportList.js
+++ PocketExportList.js
@@ -1,44 +1,40 @@
 (function (settings) {
-	"use strict";
+  "use strict";
 
-	settings = settings || {
-			item:   "#queue > .item",
-			title:  ".title",
-			link:   ".item_link",
-			insert: "#page_queue"
-		};
-
-
-	function copyToClipboard(text) {
-		var el            = document.createElement('textarea');
-		el.style.position = 'absolute';
-		el.style.left     = '-9999px';
-		el.setAttribute('readonly', '');
-		el.value = text;
-
-		document.body.appendChild(el);
-		el.select();
-		var success = document.execCommand('copy');
-		document.body.removeChild(el);
-		return success;
-	}
+  settings = settings || {
+        item:  "#queue > .item",
+        title: ".title",
+        link:  ".item_link"
+      };
 
 
-	let items = document.querySelectorAll(settings.item);
+  function copyToClipboard(text) {
+    var el            = document.createElement('textarea');
+    el.style.position = 'absolute';
+    el.style.left     = '-9999px';
+    el.setAttribute('readonly', '');
+    el.value = text;
 
-	let string = "";
+    document.body.appendChild(el);
+    el.select();
+    var success = document.execCommand('copy');
+    document.body.removeChild(el);
+    return success;
+  }
 
-	Array.prototype.forEach.call(items, (el) => {
-		let text = "[" + el.querySelector(settings.title).textContent + "]" +
-			"(" + window.location.host + el.querySelector(settings.link).getAttribute("href") + ")";
-		string += text + "\n";
-	});
+  function getList(config) {
+    let string = "";
+
+    Array.prototype.forEach.call(document.querySelectorAll(config.item), (el) => {
+      let text = "[" + el.querySelector(config.title).textContent + "]" +
+          "(" + window.location.host + el.querySelector(config.link).getAttribute("href") + ")";
+      string += text + "\n";
+    });
+
+    return string;
+  }
 
 
-	copyToClipboard(string);
-	alert("List have been copied to clipboard:\n" + string);
-
-	return string;
-
+  if (copyToClipboard(getList(settings))) alert("List have been copied to clipboard!");
 })();
 
