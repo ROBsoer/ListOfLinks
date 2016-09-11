8d07c7dec26c54511265a0230f6b1c16e63d3fa2
ROBsoer <soer.rob@gmail.com>
2016-09-11 11:04:32 +0300

    Return the list Edit alert's message.

--- PocketExportList.js
+++ PocketExportList.js
@@ -1,40 +1,45 @@
 (function (settings) {
-  "use strict";
+	"use strict";
 
 
-  settings = settings || {
-        item:  "#queue > .item",
-        title: ".title",
-        link:  ".item_link"
-      };
+	settings = settings || {
+			item:  "#queue > .item",
+			title: ".title",
+			link:  ".item_link"
+		};
 
 
-  function copyToClipboard(text) {
-    var el            = document.createElement('textarea');
-    el.style.position = 'absolute';
-    el.style.left     = '-9999px';
-    el.setAttribute('readonly', '');
-    el.value = text;
+	function copyToClipboard(text) {
+		var el            = document.createElement('textarea');
+		el.style.position = 'absolute';
+		el.style.left     = '-9999px';
+		el.setAttribute('readonly', '');
+		el.value = text;
 
-    document.body.appendChild(el);
-    el.select();
-    var success = document.execCommand('copy');
-    document.body.removeChild(el);
-    return success;
-  }
+		document.body.appendChild(el);
+		el.select();
+		var success = document.execCommand('copy');
+		document.body.removeChild(el);
+		return success;
+	}
 
-  function getList(config) {
-    let string = "";
+	function getList(config) {
+		let string = "";
 
-    Array.prototype.forEach.call(document.querySelectorAll(config.item), (el) => {
-      let text = "[" + el.querySelector(config.title).textContent + "]" +
-          "(" + window.location.host + el.querySelector(config.link).getAttribute("href") + ")";
-      string += text + "\n";
-    });
+		Array.prototype.forEach.call(document.querySelectorAll(config.item), (el) => {
+			let text = "[" + el.querySelector(config.title).textContent + "]" +
+				"(" + window.location.host + el.querySelector(config.link).getAttribute("href") + ")";
+			string += text + "\n";
+		});
 
-    return string;
-  }
+		return string;
+	}
 
-  if (copyToClipboard(getList(settings))) alert("List have been copied to clipboard!");
+	let list = getList(settings);
+
+
+	if (copyToClipboard(list)) alert("List have been copied to the clipboard!\nYou can see your list in browser's console.");
+
+	return list;
 })();
 
